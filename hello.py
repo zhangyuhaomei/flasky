345f1f49742428e1c1db109dede25b031616de14
zhangyuhaomei <2284821507@qq.com>
2016-03-21 21:22:43 +0800

    add form

--- hello.py
+++ hello.py
@@ -3,17 +3,29 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime 
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	name = None 
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data 
+		form.name.data = ''
 	return render_template('index.html',
-		current_time=datetime.utcnow())
+		form=form, name=name)
 	
 @app.route('/user/<name>')
 def user(name):
@@ -27,6 +39,9 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'),500 
 	
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
 	
 if __name__=='__main__':
 	app.run(debug=True)
